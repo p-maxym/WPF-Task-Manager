9b41f0e08ed27b7bc9f344ccef00e1f95a799596
maxym <maxym@DESKTOP-35V1EVL>
2024-07-05 03:31:10 +0300

    window name panel completed

--- WPF Task Manager/MainWindow.xaml.cs
+++ WPF Task Manager/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Google.Protobuf.WellKnownTypes;
+using MySql.Data.MySqlClient;
 using System.Data.Common;
 using System.Diagnostics;
 using System.Diagnostics.Eventing.Reader;
@@ -22,8 +23,82 @@ namespace WPF_Task_Manager
         public MainWindow()
         {
             InitializeComponent();
-            
+            SizeChanged += OnWindow_SizeChanged;
         }
 
+        // track changes in the width of the entire window
+        void OnWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.WidthChanged)
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    WindowNameBorder.BorderThickness = new Thickness(7);
+                    labelMaxmin.Content = "❐";
+                }
+                else
+                {
+                    WindowNameBorder.BorderThickness = new Thickness(0, 0, 0, 0);
+                    labelMaxmin.Content = "☐";
+                }
+            }
+        }
+
+        // handles button hovering
+        private void header_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Border? border = sender as Border;
+            if (border == null) return;
+            if (border.Name == "closeButton")
+                border.Background = Brushes.Red;
+            else
+            {
+                border.Background = new BrushConverter().ConvertFromString("#545454") as Brush;
+            }
+        }
+
+        // handles: mouse leaves the button
+        private void header_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Border? border = sender as Border;
+            if (border == null) return;
+            border.Background = new BrushConverter().ConvertFromString("#222222") as Brush;
+            border.Opacity = 1;
+        }
+
+        // handles pressing the button
+        private void header_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Border? border = sender as Border;
+            if (border == null) return;
+            if (border.Name == "closeButton")
+            {
+                border.Background = Brushes.Red;
+                border.Opacity = 0.7;
+            }
+            else
+            {
+                border.Background = new BrushConverter().ConvertFromString("#545454") as Brush;
+                border.Opacity = 0.7;
+            }
+        }
+
+        // control the action of the buttons: close, full screen, small window, minimize to taskbar
+        private void header_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Border? border = sender as Border;
+            if (border == null) return;
+            if (border.Name == "closeButton")
+                Close();
+            else if ((border.Name == "maxMinButton"))
+            {
+                if (WindowState == WindowState.Maximized)
+                    WindowState = WindowState.Normal;
+                else
+                    WindowState = WindowState.Maximized;
+            }
+            else
+                WindowState = WindowState.Minimized;
+        }
     }
 }
